47a858544eabfc109af259210995583ce35ecb59
Liubov Alieksieieva <liubovalieksieieva@gmail.com>
2024-02-15 13:18:35 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+
+  if (targetElement.closest(".menu-burger")) {
+    document.documentElement.classList.toggle("menu-open");
+    e.preventDefault();
+  }
+});
