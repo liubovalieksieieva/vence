cb177bc482ca71926e7768a97dc67619ea830bde
Liubov Alieksieieva <liubovalieksieieva@gmail.com>
2024-02-16 16:37:08 +0200

    fixed adaptive

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@ document.addEventListener("click", function (e) {
 
   if (targetElement.closest(".menu-burger")) {
     document.documentElement.classList.toggle("menu-open");
-    e.preventDefault();
+    document.body.classList.toggle("lock");
+    //  e.preventDefault();
   }
 });
 document.addEventListener("click", function (e) {
   const targetElement = e.target;
   if (targetElement.closest(".menu__body")) {
     document.documentElement.classList.remove("menu-open");
-    e.preventDefault();
+    //  e.preventDefault();
   }
 });
